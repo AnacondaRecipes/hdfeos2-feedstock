2db1928c57b35539159f06b9814b99f9b4407fb3
Michael Sarahan <msarahan@gmail.com>
2017-11-30 15:23:34 -0600

    cb3-ize

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-export CC=${PREFIX}/bin/h4cc
-export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
-export CFLAGS="-fPIC"
+# export CC=${PREFIX}/bin/h4cc
+# export CXX=${PREFIX}/bin/h4c++
+# export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
+# export CFLAGS="-fPIC"
 
 ./configure --prefix=${PREFIX} \
+            --build=${BUILD} \
+            --host=${HOST} \
             --with-hdf4=${PREFIX} \
             --with-zlib=${PREFIX} \
             --with-jpeg=${PREFIX} \
+            --enable-shared=yes \
+            --enable-static=no \
             --enable-install-include
 
 make
