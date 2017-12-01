2afc3d14e7ec190325654e8def45c9e1c6ef18e2
Michael Sarahan <msarahan@gmail.com>
2017-11-30 22:53:24 -0600

    fix automake files for linux compatibility

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
 
-# export CC=${PREFIX}/bin/h4cc
-# export CXX=${PREFIX}/bin/h4c++
-# export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
-# export CFLAGS="-fPIC"
-
+autoreconf -vfi
 ./configure --prefix=${PREFIX} \
             --build=${BUILD} \
             --host=${HOST} \
             --with-hdf4=${PREFIX} \
             --with-zlib=${PREFIX} \
             --with-jpeg=${PREFIX} \
-            --enable-shared=yes \
-            --enable-static=no \
             --enable-install-include
 
-make
-make check
+make -j${CPU_COUNT}
 make install
+make check
 
 pushd include
 make install-includeHEADERS
